83c82e6807eedaf2e96caa5ad4695cd1ce46f918
Bas Maassen <bas.maassen@adyen.com>
2020-12-03 17:48:14 +0100

    Refactored AdyenUtil, added capture files, added adyenConstants (#9)
    * Refactor AdyenUtil, Added capture classes, added constants
    * Frontend validation components

--- force-app/main/default/staticresources/adyenCheckout.js
+++ force-app/main/default/staticresources/adyenCheckout.js
@@ -1,15 +1,17 @@
-let checkout;
+const checkout = {};
+const componentsObj = {};
 
-function renderAdyenComponent(paymentMethodsResponse) {
+renderAdyenComponent = paymentMethodsResponse => {
     //TODOBAS This will be configurable in later phase. Also client key instead of originKey
     const configuration = {
         locale: "en_US",
         environment: "test",
         clientKey: "test_V34KOGIDVJHLTOSMCTHATSWM5AFOQB6M",
         paymentMethodsResponse: paymentMethodsResponse,
+        paymentMethodsConfiguration: getPaymentMethodsConfig(),
         onChange: handleOnChange,
     };
-    checkout = new AdyenCheckout(configuration);
+    checkout.adyenCheckout = new AdyenCheckout(configuration);
 
     paymentMethodsResponse.paymentMethods.forEach((pm) =>
         renderPaymentMethod(pm)
@@ -18,13 +20,24 @@ function renderAdyenComponent(paymentMethodsResponse) {
     selectFirstPaymentMethod();
 }
 
-function handleOnChange(state){
-   if(state.isValid){
-        document.getElementById('adyenStateData').value = JSON.stringify(state.data);
+getPaymentMethodsConfig = () => {
+    return {
+        card: {
+            enableStoreDetails: !isGuest,
+        }
     }
 }
 
-function renderPaymentMethod(paymentMethod){
+handleOnChange = state => {
+    const type = state.data.paymentMethod.type == "scheme" ? "card" : state.data.paymentMethod.type;
+    if (!componentsObj[type]) {
+        componentsObj[type] = {};
+    }
+    componentsObj[type].isValid = state.isValid;
+    componentsObj[type].stateData = state.data;
+}
+
+renderPaymentMethod = paymentMethod => {
     paymentMethod.type = paymentMethod.type == "scheme" ? "card" : paymentMethod.type;
     const paymentMethodsUI = document.querySelector('#paymentMethodsList');
     const container = document.createElement('div');
@@ -35,14 +48,28 @@ function renderPaymentMethod(paymentMethod){
     paymentMethodsUI.append(li);
     handleInput(paymentMethod.type);
 
-    try {
-        const node = checkout.create(paymentMethod.type).mount(container);
-    } catch (e) {
-        /* No component for payment method */
+    const node = createNode(paymentMethod.type)
+    if (node) {
+        node.mount(container);
     }
 }
 
-function configureListItem(paymentMethod, li){
+createNode = paymentMethod => {
+    if (!componentsObj[paymentMethod]) {
+        componentsObj[paymentMethod] = {};
+    }
+    try {
+        const node = checkout.adyenCheckout.create(paymentMethod)
+        componentsObj[paymentMethod].node = node;
+        return node;
+    } catch (e) {
+        /* No component for payment method */
+        componentsObj[paymentMethod].isValid = true;
+        return;
+    }
+}
+
+configureListItem = (paymentMethod, li) => {
     const liContents = `
                          <input name="brandCode" type="radio" value="${paymentMethod.type}" id="rb_${paymentMethod.type}">
                          <img class="paymentMethod_img" src="https://checkoutshopper-test.adyen.com/checkoutshopper/images/logos/medium/${paymentMethod.type}.png">
@@ -52,35 +79,55 @@ function configureListItem(paymentMethod, li){
     li.classList.add('paymentMethod');
 }
 
-function configureContainer(paymentMethodType, container) {
+configureContainer = (paymentMethodType, container) => {
     container.classList.add('additionalFields');
     container.setAttribute('id', `component_${paymentMethodType}`);
     container.setAttribute('style', 'display:none');
 }
 
-function handleInput(paymentMethodType) {
+handleInput = paymentMethodType => {
     const input = document.querySelector(`#rb_${paymentMethodType}`);
     input.onchange = (event) => {
         displaySelectedMethod(event.target.value);
     };
 }
 
-function selectFirstPaymentMethod(){
+selectFirstPaymentMethod = () => {
     const firstPaymentMethod = document.querySelector(
-    'input[type=radio][name=brandCode]',
+        'input[type=radio][name=brandCode]',
     );
     firstPaymentMethod.checked = true;
     displaySelectedMethod(firstPaymentMethod.value);
 }
 
-function displaySelectedMethod(type){
+displaySelectedMethod = type => {
+    checkout.selectedMethod = type;
     $('.additionalFields').hide();
     document
         .querySelector(`#component_${type}`)
         .setAttribute('style', 'display:block');
 }
 
-function convertToJsonObject(jsonString){
+validateComponent = () => {
+    const type = checkout.selectedMethod;
+    if (componentsObj[type].isValid) {
+        assignStateData();
+        return true;
+    }
+    componentsObj[type].node.showValidation();
+    return;
+}
+
+assignStateData = () => {
+    const type = checkout.selectedMethod;
+    const hasStateData = componentsObj[type] && componentsObj[type].stateData;
+    const stateData = hasStateData ? componentsObj[type].stateData : {paymentMethod: {type: selectedMethod}}
+    document.querySelector('#adyenStateData').value = JSON.stringify(
+        stateData,
+    );
+}
+
+convertToJsonObject = jsonString => {
     jsonString = jsonString.replace(/&quot;/g, '\"');
     return JSON.parse(jsonString);
 }
